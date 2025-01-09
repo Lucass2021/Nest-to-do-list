67f1f40e6fa84a85cafda86c4060c2e9a26e03f8
Lucas Dias <lucas.allx@hotmail.com>
2025-01-09 17:09:27 -0300

    Nest: setup swagger initial

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Validated UUID in findOneDTO
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('To-do List')
+    .setDescription('To-do List API')
+    .setVersion('1.0')
+    .addTag('to-do')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
