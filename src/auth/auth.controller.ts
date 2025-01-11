d9ecae38156f06d3b69223feb1182ff6961c15b7
Lucas Dias <lucas.allx@hotmail.com>
2025-01-11 11:04:50 -0300

    Nest: auth controller swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { Request } from 'express';
 import { AuthService } from './auth.service';
 import { LocalGuard } from './guards/local.guard';
 import { JwtAuthGuard } from './guards/jwt.guard';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 @ApiBearerAuth()
@@ -12,12 +12,30 @@ export class AuthController {
 
   @Post('/login')
   @UseGuards(LocalGuard)
+  @ApiOperation({
+    summary: 'Login',
+    description: 'Endpoint to login',
+  })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: { type: 'string', example: 'user@example.com' },
+        password: { type: 'string', example: 'securePassword123!' },
+      },
+      required: ['email', 'password'],
+    },
+  })
   login(@Req() req: Request) {
     return this.authService.login(req);
   }
 
   @Get('/status')
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: 'User JWT content',
+    description: 'Endpoint to get user JWT content',
+  })
   status(@Req() req: Request) {
     return req.user;
   }
